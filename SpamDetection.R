73425dbdf4f349113b213cddc89d1eb3e2890bb0
Nimish <nimishbajaj@hotmail.com>
2017-04-02 03:51:50 +0530

    predictions

--- SpamDetection.R
+++ SpamDetection.R
@@ -31,24 +31,24 @@ set.seed(123)
 spamRf = randomForest(spam ~ ., data=train)
 
 #evaluating models
-probLog = predict(spamLog, newdata=train, type = "response")
-probCart = predict(spamCart, newdata = train, type = "prob")
-probRf = predict(spamRf, newdata = train, type = "prob")
+probLog = predict(spamLog, newdata=test, type = "response")
+probCart = predict(spamCart, newdata = test, type = "prob")
+probRf = predict(spamRf, newdata = test, type = "prob")
 
 summary(spamLog)
-sum(diag(table(train$spam, probLog >= 0.5)))/nrow(train) #Accuracy of Logistic Regression
+sum(diag(table(test$spam, probLog >= 0.5)))/nrow(test) #Accuracy of Logistic Regression
 
 library(ROCR)
-pred = prediction(probLog, train$spam)
+pred = prediction(probLog, test$spam)
 pref = performance(pred, "auc")
-auc = as.numeric(pref@y.values)
+as.numeric(pref@y.values)
 
-sum(diag(table(train$spam, probCart[,2] >= 0.5)))/nrow(train)
-auc = as.numeric(performance(prediction(probCart[,2],train$spam),"auc")@y.values)
+sum(diag(table(test$spam, probCart[,2] >= 0.5)))/nrow(test)
+as.numeric(performance(prediction(probCart[,2],test$spam),"auc")@y.values)
 
-sum(diag(table(train$spam, probRf[,2] >= 0.5)))/nrow(train)
-auc = as.numeric(performance(prediction(probRf[,2],train$spam),"auc")@y.values)
-auc
+sum(diag(table(test$spam, probRf[,2] >= 0.5)))/nrow(test)
+as.numeric(performance(prediction(probRf[,2],test$spam),"auc")@y.values)
+
 
 
 
